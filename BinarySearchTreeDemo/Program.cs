3738aaea921aeab3842848f4780c3540541501ab
pbishal <pbishal60@gmail.com>
2021-05-09 23:04:21 +0530

    Able to create A BST by adding 56,30 and 70

--- BinarySearchTreeDemo/Program.cs
+++ BinarySearchTreeDemo/Program.cs
@@ -11,6 +11,12 @@ namespace BinarySearchTreeDemo
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome Binary Search Tree");
+            Console.WriteLine("Inserted nodes are : ");
+            BinarySearchTree<int> binarySearchTree = new BinarySearchTree<int>(56);
+            binarySearchTree.InsertNode(30);
+            binarySearchTree.InsertNode(70);
+            binarySearchTree.DisplayMethod();
+            Console.ReadLine();
         }
     }
 }
